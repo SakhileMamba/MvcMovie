83137cc75da620163f692c13738a91ca94e769d6
Sakhile Mamba <sakhymamba@gmail.com>
2026-09-18 09:16:20 +0200

    Refactor code structure for improved readability and maintainability

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951526516ef96e003bb473d5638dabf70948a749")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42dfc7d30411811b7d0bcf8882a3a6903b02568d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
