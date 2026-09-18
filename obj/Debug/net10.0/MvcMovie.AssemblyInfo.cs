42dfc7d30411811b7d0bcf8882a3a6903b02568d
Sakhile Mamba <sakhymamba@gmail.com>
2026-09-18 06:56:34 +0200

    Update project.nuget.cache with new package files and vulnerability warnings

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d504bf3e95f116453bf21ec308f4c0dab87e15ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951526516ef96e003bb473d5638dabf70948a749")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
